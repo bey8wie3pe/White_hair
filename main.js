04fec38821fcd6a6ab4538b5930439f23d30fa94
bey8wie3pe <94910229+bey8wie3pe@users.noreply.github.com>
2022-07-21 15:56:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,28 +2,34 @@
 let n = 0
 //プラスを押した場合
 function btnclick(){
-    n = n + 1
+    n = n + 1;
     if (n < 0){
         n = 0;
     }
     let testinfo = document.getElementById("test");
     testinfo.innerHTML = `${n}本目`;
+    let salary = document.getElementById("salary");
+    let salary_read = salary.value;
+    let money = document.getElementById("money");
+    money.innerHTML = `${n*salary_read}円`;
 }
 //マイナスを押した場合
 function btn_click(){
-    n = n - 1
+    n = n - 1;
     if (n < 0){
         n = 0;
     }
     let testinfo = document.getElementById("test");
     testinfo.innerHTML = `${n}本目`;
+    let salary = document.getElementById("salary");
+    let salary_read = salary.value;
+    let money = document.getElementById("money");
+    money.innerHTML = `${n*salary_read}円`;
 }
 //終了ボタンを押したらcookieを埋め込む
 function finish(){
     document.cookie = 'siraga='+n ;'expires=Tue, 19 Jan 2038 03:14:07 GMT'
-}
-function cookie(){
-    let a = document.siraga
+
 }
 //全てのcookieを取り出す
 let cookies = document.cookie; 
@@ -34,9 +40,11 @@ for(let c of cookiesArray){
     //さらに=で分割して配列に
     let cArray = c.split('='); 
      // 取り出したいkeyと合致したら以下のものを実行する
-    if( cArray[0] == 'siraga'){
+     if( cArray[0] == 'siraga'){
         let hair = document.getElementById("hair");
         hair.innerHTML = `前回は${cArray}本抜きました。`;
     }
 }
+
+
 
